b9ef563c97788a95b78ee720b5a67acbf572e274
Documenter.jl <documenter@juliadocs.github.io>
2023-04-20 18:34:02 +0000

    build based on ec28cb7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Synthdid","category":"page"},{"location":"#Synthdid.","page":"Home","title":"Synthdid.","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Synthdid.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Synthdid]","category":"page"}]
+}
